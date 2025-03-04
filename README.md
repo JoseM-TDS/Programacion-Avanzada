87a4131144eae0baf05a0d0306ebc9a27784a135
Jose Melendez <23001327@galileo.edu>
2025-03-03 19:20:41 -0600

    Primer commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Programacion-Avanzada
+Proyecto de aplicación web con Next.Js, Express.Js y MongoDB
---
+++ routes/index.js
@@ -0,0 +1,16 @@
+var express = require('express');
+var router = express.Router();
+const Habit = require('../modelo/habit');
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+router.post('/habits', async function(req, res, next){
+  const {title, description} = req.body;
+  const habit = new Habit({title, description});
+  await habit.save();
+  res.json(habit);
+})
+module.exports = router;
