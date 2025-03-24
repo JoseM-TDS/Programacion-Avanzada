65402a058d4c7f761c4fcc1c808049360448882c
Jose Melendez <23001327@galileo.edu>
2025-03-24 00:46:19 -0600

    Feat: Implementacion de registro y login de usuarios con hash

--- HabitsApp-Backend/app.js
+++ HabitsApp-Backend/app.js
@@ -9,6 +9,10 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
+app.use(cors({
+  origin:'http://localhost:3000',
+  Credentials: true
+}))
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
