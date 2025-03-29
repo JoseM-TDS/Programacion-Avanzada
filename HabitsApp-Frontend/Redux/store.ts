f2436ffa021b06402a7d4166e21da1d81e3726a0
Jose Melendez <23001327@galileo.edu>
2025-03-29 00:20:48 -0600

    Feat: Integracion de estado User en Redux

--- HabitsApp-Frontend/Redux/store.ts
+++ HabitsApp-Frontend/Redux/store.ts
@@ -1,10 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 import habitReducer from '../features/habit/habitSlice';
+import userReducer from '../features/users/userSlice';
 
 export const makeStore = () => {
     return configureStore({
         reducer: {
-            habits: habitReducer
+            habits: habitReducer,
+            user: userReducer
         }
     })
 }
--- HabitsApp-Frontend/features/habit/habitSlice.ts
+++ HabitsApp-Frontend/features/habit/habitSlice.ts
@@ -1,4 +1,4 @@
-import { createSlice, createAsyncThunk, isRejectedWithValue } from "@reduxjs/toolkit";
+import { createSlice, createAsyncThunk} from "@reduxjs/toolkit";
 import { fetchHabits } from "./habitAPI";
 
 type Habit = {
